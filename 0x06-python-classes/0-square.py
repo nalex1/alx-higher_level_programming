fc9a4babb1e4954d606ab22fe7d213660cd31a80
root <alxnkuru@gmail.com>
2024-04-03 14:39:00 -0700

    class and object

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a module for square"""
+
+
+class Square:
+    """Represents a Square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module interpreter"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """constructor object.
+        Args:
+        size: length of the side of the square
+        """
+        self.__size = size
